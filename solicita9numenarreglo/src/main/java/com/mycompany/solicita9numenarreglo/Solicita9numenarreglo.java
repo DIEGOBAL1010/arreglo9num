b3c41da101afcdab31e8047c9a414a9caa6d57bd
DIEGOBAL1010 <145385058+DIEGOBAL1010@users.noreply.github.com>
2023-09-26 09:27:32 -0600

    Add files via upload

---
+++ solicita9numenarreglo/src/main/java/com/mycompany/solicita9numenarreglo/Solicita9numenarreglo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solicita9numenarreglo;
+
+/**
+ *
+ * @author User
+ */
+public class Solicita9numenarreglo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
